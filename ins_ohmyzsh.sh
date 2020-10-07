712b21a35adcc377e993f1318cd3cd4257131722
ilovekuchen <ilovekuchen@gmail.com>
2020-10-07 16:41:19 +0200

    fuzzyfinder

--- ins_ohmyzsh.sh
+++ ins_ohmyzsh.sh
@@ -6,3 +6,7 @@ sudo apt --assume-yes install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' .zshrc
+echo ZSH_DISABLE_COMPFIX=true >> .zshrc         
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+sudo ~/.fzf/install
+
