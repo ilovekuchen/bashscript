a5a577bce7020a4173cf625f25027c4746a1d7b9
ilovekuchen <ilovekuchen@gmail.com>
2020-10-07 16:03:45 +0200

    nix vergessen

---
+++ ins_ohmyzsh.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+cd
+sudo apt --assume-yes install git
+sudo apt --assume-yes install wget curl git -y
+sudo apt --assume-yes install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' .zshrc
